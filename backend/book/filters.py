8305652171f6b877f4f20bdf9cac472a6ca060ba
garciamendes <garciaprog0101@gmail.com>
2024-03-25 19:45:18 -0300

    filters

--- backend/book/filters.py
+++ backend/book/filters.py
@@ -1,11 +1,20 @@
 # Third party
-from django_filters.rest_framework import FilterSet
+from django_filters import FilterSet
+from django_filters import CharFilter
 
 # Local
 from .models import Book
 
 class BookFilters(FilterSet):
+    categories = CharFilter(method='categories_filter')
 
     class Meta:
         model = Book
         fields = ['categories']
+
+    def categories_filter(self, queryset, name, values):
+        if not values:
+            return queryset
+
+        categories_to_filters = values.split(',')
+        return queryset.filter(categories__slug__in=categories_to_filters)
